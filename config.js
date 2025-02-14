411014736095b2ba6376432b93713de4b23c54f4
Donquixchotte <diazhacker95@gmail.com>
2025-02-14 01:57:15 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: [6493353976], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7572177283:AAFGIjI4HTNeC9mO8ap3dTR8yugnVUY1Ydc", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// CRACK BY VINNTHEXDC 
+// https://whatsapp.com/channel/0029VavOzFy8KMqqMvMnOW1k
